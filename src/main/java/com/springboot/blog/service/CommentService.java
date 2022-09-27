0235ede49fe09096fb185862cd475a7fe14df7f1
VijayHYadav <vizzyadav1997@gmail.com>
2022-09-27 10:09:56 +0530

    008 Update Comment By Id REST API

--- src/main/java/com/springboot/blog/service/CommentService.java
+++ src/main/java/com/springboot/blog/service/CommentService.java
@@ -5,9 +5,39 @@ import java.util.List;
 import com.springboot.blog.payload.CommentDto;
 
 public interface CommentService {
+	/**
+	 * Use to create post.
+	 * 
+	 * @param postId
+	 * @param commentDto
+	 * @return CommentDto
+	 */
 	CommentDto createComment(long postId, CommentDto commentDto);
 
+	/**
+	 * Use to get comments by post id.
+	 * 
+	 * @param postId
+	 * @return List<CommentDto>
+	 */
 	List<CommentDto> getCommentsByPostId(long postId);
 
+	/**
+	 * Use to get comments by post id and comment id
+	 * 
+	 * @param postId
+	 * @param commentId
+	 * @return CommentDto
+	 */
 	CommentDto getCommentById(Long postId, Long commentId);
+
+	/**
+	 * Use to update comment
+	 * 
+	 * @param postId
+	 * @param commentId
+	 * @param commentRequest
+	 * @return CommentDto
+	 */
+	CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest);
 }
--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -72,6 +72,29 @@ public class CommentServiceImpl implements CommentService {
 		return mapToDto(comment);
 	}
 
+	@Override
+	public CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest) {
+
+		// retrieve post entity by id
+		Post post = postRepository.findById(postId)
+				.orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+		// retrieve comment by id
+		Comment comment = commentRepository.findById(commentId)
+				.orElseThrow(() -> new ResourceNotFoundException("Comment", "id", commentId));
+
+		if (!comment.getPost().getId().equals(post.getId())) {
+			throw new BlogApiException(HttpStatus.BAD_REQUEST, "Comment does not belongs to post");
+		}
+
+		comment.setName(commentRequest.getName());
+		comment.setEmail(commentRequest.getEmail());
+		comment.setBody(commentRequest.getBody());
+
+		Comment updateComment = commentRepository.save(comment);
+		return mapToDto(updateComment);
+	}
+
 	private CommentDto mapToDto(Comment comment) {
 		CommentDto commentDto = new CommentDto();
 		commentDto.setId(comment.getId());
