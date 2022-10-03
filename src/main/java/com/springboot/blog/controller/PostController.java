9569e01c116b5bd5bebfdb31e2daf5484a4388a5
VijayHYadav <vizzyadav1997@gmail.com>
2022-10-03 09:32:04 +0530

    002 Validate Create Post and Update Post REST API Request

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,7 +33,7 @@ public class PostController {
 
 	// create blog post
 	@PostMapping
-	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+	public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
 		return new ResponseEntity<PostDto>(postService.createPost(postDto), HttpStatus.CREATED);
 	}
 
@@ -56,7 +58,7 @@ public class PostController {
 
 	// Update post by id
 	@PutMapping("/{id}")
-	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+	public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
 		PostDto postResponse = postService.updatePost(postDto, id);
 		return new ResponseEntity<>(postResponse, HttpStatus.OK);
 	}
--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,6 +2,9 @@ package com.springboot.blog.payload;
 
 import java.util.Set;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,21 @@ import lombok.NoArgsConstructor;
 
 public class PostDto {
 	private Long id;
+
+	// title should not be null or empty
+	// title should have at least 2 characters
+	@NotEmpty
+	@Size(min = 2, message = "Post title should have at least 2 characters")
 	private String title;
+
+	// post description should be null or empty
+	// post description should have at least 10 characters
+	@NotEmpty
+	@Size(min = 10, message = "Post description should have at least 10 characters")
 	private String description;
+
+	// post content should not be null or empty
+	@NotEmpty
 	private String content;
 	private Set<CommentDto> comments;
 }
