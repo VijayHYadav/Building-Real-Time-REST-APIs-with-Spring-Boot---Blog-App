5a69cdab57ec48ee3b1d4f572be1f643e9dc3b1a
VijayHYadav <vizzyadav1997@gmail.com>
2022-10-03 10:16:47 +0530

    003 Customizing Validation Response

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.springboot.blog.exception;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -41,5 +45,36 @@ public class GlobalExceptionHandler {
 				webRequest.getDescription(false));
 
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+//	@Override
+//	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+//			HttpHeaders headers, HttpStatus status, WebRequest request) {
+//
+//		Map<String, String> errors = new HashMap<String, String>();
+//
+//		ex.getBindingResult().getAllErrors().forEach((error) -> {
+//			String fieldName = ((FieldError) error).getField();
+//			String message = error.getDefaultMessage();
+//			errors.put(fieldName, message);
+//		});
+//
+//		return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+//	}
+
+	// handle global exceptions
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+			WebRequest webRequest) {
+
+		Map<String, String> errors = new HashMap<String, String>();
+
+		exception.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String message = error.getDefaultMessage();
+			errors.put(fieldName, message);
+		});
+
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 }
