9a46e213a41dce0c2c76108f4d62fc2dab767a25
VijayHYadav <vizzyadav1997@gmail.com>
2022-10-04 07:23:24 +0530

    002 Implementing Basic Authentication using Spring Security

---
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.blog.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+
+		return http.build();
+	}
+
+}
